f4475602664e2dd2cc0ecd36929a8f147d017fd8
Nok Lam Chan <nok.lam.chan@quantumblack.com>
2024-05-31 12:54:47 +0100

    v1 - Add config loader

---
+++ run_v1.py
@@ -0,0 +1,39 @@
+if __name__ == "__main__":
+    print("Start Pipeline")
+    from minikedro.pipelines.data_processing.nodes import (
+        create_model_input_table,
+        preprocess_companies,
+        preprocess_shuttles,
+    )
+    from rich.logging import RichHandler
+    import logging
+    import pandas as pd
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        handlers=[RichHandler()],
+    )
+    logger = logging.getLogger("minikedro")
+
+    config = {
+        "companies": {"filepath": "data/01_raw/companies.csv"},
+        "reviews": {"filepath": "data/01_raw/reviews.csv"},
+        "shuttles": {"filepath": "data/01_raw/shuttles.xlsx"},
+    }
+    from minikedro.v1 import ConfigLoader
+
+    config_loader = ConfigLoader(config)
+
+    companies = pd.read_csv(config_loader["companies"]["filepath"])
+    reviews = pd.read_csv(config_loader["reviews"]["filepath"])
+    shuttles = pd.read_excel(config_loader["shuttles"]["filepath"])
+
+    logger.info("Running preprocess_companies")
+    processed_companies = preprocess_companies(companies)
+    logger.info("Running shuttles")
+    processed_shuttles = preprocess_shuttles(shuttles)
+    logger.info("Running create_model_input_table")
+    model_input_table = create_model_input_table(
+        processed_shuttles, processed_companies, reviews
+    )
